971e710669fe3eb83290f4b901bc3550b0a0cb5b
andreadldev <andrea.delisi2@gmail.com>
2022-11-30 15:24:37 +0100

    --

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import { store } from './js/store';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+    components: {
+        HelloWorld
+    },
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <HelloWorld/>
+</template>
+
+<style lang="scss">
+@import "./css/style.scss";
+</style>

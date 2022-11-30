7c70cbcc9e140c202619f2af889a2f76bdaee76c
andreadldev <andrea.delisi2@gmail.com>
2022-11-30 16:38:57 +0100

    test risposta api

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,29 @@
 <script>
+import axios from "axios";
+import { store } from '../js/store';
+
+export default {
+    data() {
+        return {
+            store,
+            key: '01ec531f2d0136f3747724d7db11da55'
+        }
+    },
+    methods: {
+        search() {
+            axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${key}&query=Jack+Reacher`).then((resp) => {
+            store.example = resp.data;
+            console.log(store.example)
+            }
+        );
+        }
+    }
+}
 </script>
 
 <template>
-    <h1>Hello World</h1>
+    <input type="text">
+    <button @click="search()">premi</button>
 </template>
 
 <style lang="scss" scoped>

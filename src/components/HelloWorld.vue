a51b4e5a31ebd2d3843d89db599fa7fcccaba679
andreadldev <andrea.delisi2@gmail.com>
2022-11-30 16:47:15 +0100

    Test api 2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,12 +6,13 @@ export default {
     data() {
         return {
             store,
-            key: '01ec531f2d0136f3747724d7db11da55'
+            key: '01ec531f2d0136f3747724d7db11da55',
+            movie: 'Back to the future'
         }
     },
     methods: {
         search() {
-            axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${key}&query=Jack+Reacher`).then((resp) => {
+            axios.get("https://api.themoviedb.org/3/search/movie?api_key="+this.key+"&query="+this.movie).then((resp) => {
             store.example = resp.data;
             console.log(store.example)
             }

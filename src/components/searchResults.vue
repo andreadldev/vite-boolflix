be9f83b585636c1d2f807980607f1d6e5edd101f
andreadldev <andrea.delisi2@gmail.com>
2022-12-02 12:29:21 +0100

    separazione risultati film e serie

--- src/components/searchResults.vue
+++ src/components/searchResults.vue
@@ -30,43 +30,75 @@ export default {
             else if (lang) {
                 return lang.toUpperCase();
             }
-        },
-        mediaType(data) {
-            if (data.media_type == 'movie') {
-                return 'Film'
-            }
-            else {
-                return 'Serie TV'
-            }
         }
     }
 }
 </script>
-
 <template>
     <section>
-        <div v-if="store.productList.results != 0" v-for="product in store.productList.results">
-            <ul v-if="product.media_type != 'person'">
-                <li><h3>{{product.title}} {{product.name}}</h3></li>
-                <li>{{this.mediaType(product)}}</li>
-                <li>Titolo originale: {{product.original_title}} {{product.original_name}}</li>
-                <li class="lang">
-                    <span>Lingua:</span>
-                    <img :src=(store.info.flag.src+this.upperCase(product.original_language)+store.info.flag.type) width="24"/>
-                </li>
-                <div>
-                    <font-awesome-icon v-for="n in Math.ceil(product.vote_average / 2)" icon="fa-solid fa-star"/>
-                    <font-awesome-icon v-for="n in 5 - Math.ceil(product.vote_average / 2)" icon="fa-regular fa-star"/>
-                </div>
-                <li><img :src=store.info.poster+product.poster_path alt="..."></li>
-            </ul>
+        <h1 v-if="(store.movieList.results.length > 0)">FILM</h1>
+        <div class="row">
+            <div class="card" v-for="movie in store.movieList.results">
+                <ul>
+                    <li><img class="poster" :src=store.info.poster+movie.poster_path alt="..."></li>
+                    <li><h3>{{movie.title}}</h3></li>
+                    <li>Titolo originale: {{movie.original_title}}</li>
+                    <li class="lang">
+                        <span>Lingua:</span>
+                        <img :src=(store.info.flag.src+this.upperCase(movie.original_language)+store.info.flag.type) width="24"/>
+                    </li>
+                    <div>
+                        <font-awesome-icon v-for="n in Math.ceil(movie.vote_average / 2)" icon="fa-solid fa-star"/>
+                        <font-awesome-icon v-for="n in 5 - Math.ceil(movie.vote_average / 2)" icon="fa-regular fa-star"/>
+                    </div>
+                </ul>
+            </div>
+        </div>
+
+        <h1 v-if="(store.tvList.results.length > 0)">SERIE TV</h1>
+        <div class="row">
+            <div class="card" v-if="store.tvList.results != 0" v-for="series in store.tvList.results">
+                <ul>
+                    <li><img class="poster" :src=store.info.poster+series.poster_path alt="..."></li>
+                    <li><h3>{{series.name}}</h3></li>
+                    <li>Titolo originale: {{series.original_name}}</li>
+                    <li class="lang">
+                        <span>Lingua:</span>
+                        <img :src=(store.info.flag.src+this.upperCase(series.original_language)+store.info.flag.type) width="24"/>
+                    </li>
+                    <div>
+                        <font-awesome-icon v-for="n in Math.ceil(series.vote_average / 2)" icon="fa-solid fa-star"/>
+                        <font-awesome-icon v-for="n in 5 - Math.ceil(series.vote_average / 2)" icon="fa-regular fa-star"/>
+                    </div>
+                </ul>
+            </div>
         </div>
     </section>
 </template>
 
 <style lang="scss" scoped>
 section {
-    margin-top: 60px;
+    padding-top: 60px;
+}
+
+.row {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+.hide {
+    width: 0;
+    height: 0;
+    opacity: 0;
+}
+.card {
+    text-align: center;
+    width: calc(100% / 6);
+}
+
+.poster {
+    width: calc(100% - 25px);
+    max-height: 400px;
 }
 
 .lang {
--- src/js/store.js
+++ src/js/store.js
@@ -2,7 +2,8 @@ import { reactive } from "vue";
 import axios from "axios";
 
 export const store = reactive({
-    productList: [],
+    movieList: [],
+    tvList: [],
     key: '01ec531f2d0136f3747724d7db11da55',
     inputField: '',
     info: {
@@ -10,14 +11,19 @@ export const store = reactive({
             src: "https://www.countryflagicons.com/FLAT/24/",
             type: '.png'
         },
-        poster: 'https://image.tmdb.org/t/p/w92/'
+        poster: 'https://image.tmdb.org/t/p/w300/'
     },
     search() {
-        axios.get("https://api.themoviedb.org/3/search/multi?api_key="+store.key+"&language=it"+"&query="+store.inputField).then((resp) => {
-        store.productList = resp.data;
-        console.log(store.productList.results)
-        }
-    )
+        axios.get("https://api.themoviedb.org/3/search/movie?api_key="+store.key+"&language=it"+"&query="+store.inputField).then((resp) => {
+            store.movieList = resp.data;
+            console.log(resp.data.results)
+            }
+        ),
+        axios.get("https://api.themoviedb.org/3/search/tv?api_key="+store.key+"&language=it"+"&query="+store.inputField).then((resp) => {
+            store.tvList = resp.data;
+            console.log(resp.data.results)
+            }
+        )
     },
 
 })
